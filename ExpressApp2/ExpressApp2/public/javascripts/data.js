39cccfa0b00d60b50d9caff1de2809e5a9a3ec76
debugmodedotnet <debugmode@outlook.com>
2014-01-23 08:18:35 +0530

    Added Source Code

---
+++ ExpressApp2/ExpressApp2/public/javascripts/data.js
@@ -0,0 +1,67 @@
+﻿var product =
+[
+ {
+   "id"         :1 ,
+  "productName" : "Pen",
+ "productPrice" : "200",
+ 'productStock' : "false"
+ },
+ {
+  "id" :2 ,
+  "productName" : "Pencil",
+ "productPrice" : "200",
+ "productStock" : "false"
+ },
+ ];
+
+
+exports.getProducts = function(req,res){
+    
+       res.send(product);
+    };
+
+exports.addProduct = function(req,res)
+{       
+       var data = req.body;
+       product.push(data);     
+       res.send(product);
+}
+
+exports.deleteProduct = function(req,res)
+{
+    
+    var id = parseInt(req.params.id)-1; 
+    var itemdeleted = product.splice(id,1)
+    if(itemdeleted===undefined)
+        {
+            res.send("Not Deleted");
+        }
+    else
+      {
+         ;
+         res.send(product);
+     }
+}
+       
+
+exports.updateProduct = function(req,res)
+{
+    var id = parseInt(req.params.id)-1;
+    var productToUpdate = product[id];  
+    var data = req.body;
+    
+    if(productToUpdate===undefined)
+        {
+
+            res.send("Not Updated");
+        }
+    else
+      {
+          productToUpdate.productName = data.productName;
+          productToUpdate.productPrice = data.productPrice;
+          productToUpdate.productStock = data.productStock;
+
+     res.send(product);
+     }      
+    
+}
